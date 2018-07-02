2e9d9c48097377e38f1aacf324d750a131d0567f
niubilityoyr <1364782109@qq.com>
2018-07-02 23:40:36 +0800

    提交userController文件。

---
+++ src/main/java/com/wy/UserController.java
@@ -0,0 +1,13 @@
+package com.wy;
+
+/**
+ * Create by 欧阳荣
+ * 2018/7/2 23:31
+ */
+public class UserController {
+
+    public static void main(String[] args) {
+        System.out.println("my is userController");
+    }
+
+}
